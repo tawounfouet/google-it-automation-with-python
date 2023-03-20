6948d122352121f6dce6f79db2eb1d636657de08
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-03-20 11:32:23 +0100

    Add a check reboot function

--- all_checks.py
+++ all_checks.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+import os
+
+
+def check_reboot():
+    """Return True if the computer has a pending reboot."""
+    return os.path.exist("/run/reboot-required")
+
+
 def main():
     pass
 
